114ef1ac03d2ee72e73e925cf5cf9960d5b952ad
jeezy <jeezy@bitmessage.ch>
2018-05-01 14:22:26 +0200

    new file:   checkpull.sh 	deleted:    checkunspent.sh 	new file:   maskhex.sh

---
+++ checkpull.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+GR='\033[0;32m'
+NC='\033[0m' # No Color
+
+printf "${GR}>>>>>>> CHECKING KOMODO${NC}\n"
+cd /home/$USER/komodo/src
+printf "${GR}>>>>>>> STASH${NC}\n"
+git stash
+printf "${GR}>>>>>>> CHECKOUT DEV${NC}\n"
+git checkout dev
+printf "${GR}>>>>>>> PULL${NC}\n"
+git pull
+
+printf "${GR}>>>>>>> CHECKING IGUANA${NC}\n"
+cd /home/$USER/SuperNET/iguana
+printf "${GR}>>>>>>> STASH${NC}\n"
+git stash
+printf "${GR}>>>>>>> CHECKOUT DEV${NC}\n"
+git checkout dev
+printf "${GR}>>>>>>> PULL${NC}\n"
+git pull
--- checkunspent.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-bitcoin-cli listunspent | grep 0.0005 | wc
-#bitcoin-cli listunspent | grep 0.0001 | wc
