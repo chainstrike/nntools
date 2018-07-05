f0968eb3a5e9b6d68d1cf54b4a0141915c99a135
Team ChainStrike <chainstrike@protonmail.com>
2018-07-06 01:57:41 +1000

    Commit disperse.pl changes, which were removed with a git stash

--- disperse.pl
+++ disperse.pl
@@ -8,9 +8,11 @@ my %addresses = (
 	# address => percent
 	"RGZBQh4AqziCZ6rS8ps1kLqD7ribbCpv9a" => .40, # jeezy
 	"RBSEv7nJ1wciriVyLFWotQ8tBvS2rKwYtz" => .40, # duke
-	"AIRDROP"		             => .10, # airdrop
-	"PROJECTS"		             => .10, # projects
+	"RVwS8ghZtR89QxLXPGPJtqZEQ6KmfP7WEZ" => .10, # airdrop
+	"RF79i22W3YXMGsjzBLXNxU5KvWe31dG5pQ" => .10, # projects
 );
+
+my $self = "RXrQPqU4SwARri1m2n7232TDECvjzXCJh4";
 
 # leave 11 and disperse rest
 my $reserve = 11;
@@ -22,4 +24,8 @@ if ($disperse > 0) {
 	my $cmd = "$cli sendtoaddress $addr $amount";
 	print "$cmd\n";
    }
+   my $cmd = "$cli sendtoaddress $self 10.0";
+   print "$cmd\n";
+   $cmd = $ENV{HOME} . "/nntools/autosplit.sh";
+   print "sleep 180; $cmd\n";
 }
