25f2ef9ee547765c7ccd90265e0a6faa414099c4
Team ChainStrike <chainstrike@protonmail.com>
2018-08-02 04:53:17 +1000

    NN connect script

---
+++ nn_ips.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+#my $traceroute = "traceroute -I -n";
+my $traceroute = "traceroute -n";
+my $ips = {};
+while (<>) {
+	my $ip;
+	if ( m!NN_CONNECT to \(tcp://(.*):17775\)! ) {
+		$ip = $1;
+		my $cmd = "$traceroute $ip";
+		print "Running $cmd ...";
+		my $out = qx{$cmd};
+		#print "Hops: $out\n";
+		my $hops = $out =~ tr/\n//;
+		$hops--;
+		print "$hops hops\n";
+		$ips->{$ip} = $hops;
+	}
+}
+#my @sorted = map  { $_->[0] } 
+#			 sort { $a->[1] cmp $b->[1] }
+#			 map  { [$_, $ips->{$_} ]   } keys %$ips;
+my @sorted = sort { $ips->{$a} <=> $ips->{$b} } keys %$ips;
+
+print "Closest NN IPs:\n";
+print join("\n", @sorted) . "\n";
