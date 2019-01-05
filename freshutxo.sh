a13a28edf2c456c982dc68762426575f973302fb
jeezy <jeezy@bitmessage.ch>
2019-01-05 10:00:54 +0100

    modified:   freshutxo.sh 	modified:   nnconnect.txt

--- freshutxo.sh
+++ freshutxo.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 cd ~/komodo/src
+./komodo-cli sendtoaddress RCA8H1npFPW5pnJRzycF8tFEJmn6XZhD4j 0.1
+sleep 3
+./komodo-cli sendtoaddress RCA8H1npFPW5pnJRzycF8tFEJmn6XZhD4j 0.5
+sleep 3
 ./komodo-cli sendtoaddress RCA8H1npFPW5pnJRzycF8tFEJmn6XZhD4j 1
-sleep 1
+sleep 3
 ./komodo-cli sendtoaddress RCA8H1npFPW5pnJRzycF8tFEJmn6XZhD4j 3
-sleep 1
-./komodo-cli sendtoaddress RCA8H1npFPW5pnJRzycF8tFEJmn6XZhD4j 5
-sleep 1
-./komodo-cli sendtoaddress RCA8H1npFPW5pnJRzycF8tFEJmn6XZhD4j 7
