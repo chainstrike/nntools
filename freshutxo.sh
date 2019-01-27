f008d75a547d35e2d4712fbe028c700ca10df6bf
jeezy <jeezy@bitmessage.ch>
2019-01-27 11:55:55 +0100

    modified:   freshutxo.sh

--- freshutxo.sh
+++ freshutxo.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 cd ~/komodo/src
-./komodo-cli sendtoaddress RCA8H1npFPW5pnJRzycF8tFEJmn6XZhD4j 0.1
+./komodo-cli sendtoaddress RCA8H1npFPW5pnJRzycF8tFEJmn6XZhD4j 0.11
 sleep 3
-./komodo-cli sendtoaddress RCA8H1npFPW5pnJRzycF8tFEJmn6XZhD4j 0.5
+./komodo-cli sendtoaddress RCA8H1npFPW5pnJRzycF8tFEJmn6XZhD4j 0.51
 sleep 3
-./komodo-cli sendtoaddress RCA8H1npFPW5pnJRzycF8tFEJmn6XZhD4j 1
-sleep 3
-./komodo-cli sendtoaddress RCA8H1npFPW5pnJRzycF8tFEJmn6XZhD4j 3
+./komodo-cli sendtoaddress RCA8H1npFPW5pnJRzycF8tFEJmn6XZhD4j 1.11
+#sleep 3
+#./komodo-cli sendtoaddress RCA8H1npFPW5pnJRzycF8tFEJmn6XZhD4j 3
