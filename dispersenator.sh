f491a80833917603ad1a78834e98daad9566a978
Team ChainStrike <chainstrike@protonmail.com>
2019-04-22 19:06:53 +1000

    Add disperse script

---
+++ dispersenator.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+
+DATE=`date +%s`
+LOG=disperse/disperse-$DATE.log
+echo "Logging to $LOG"
+
+./disperse.pl > disperse/disperse-$DATE.sh
+echo "Created disperse/disperse-$DATE.sh:"
+cat disperse/disperse-$DATE.sh
+echo "Dispersing funds!"
+bash disperse/disperse-$DATE.sh &> $LOG &
+cat $LOG
