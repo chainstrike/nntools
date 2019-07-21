fbba793ae360a74ec75c642f5605eb8504aa4957
jeezy <j33zy@pm.me>
2019-07-21 12:33:05 +0200

    modified:   as_decker.sh 	new file:   cleanwallets.sh 	new file:   coinlist

---
+++ cleanwallets.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# @author webworker01
+#scriptpath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+source coinlist
+source $HOME/node.conf
+
+#echo $komodo_cli
+
+dt=$(date '+%Y-%m-%d %H:%M:%S');
+
+cleanerremoved=$($komodo_cli cleanwallettransactions | jq -r .removed_transactions)
+#if ($cleanerremoved > 0); then
+    echo "$dt [cleanwallettransactions] KMD - Removed $cleanerremoved transactions" >> $nntoolslogfile
+#fi
+
+if (( thirdpartycoins < 1 )); then
+    for coins in "${coinlist[@]}"; do
+        coin=($coins)
+        if [[ ! ${ignoreacs[*]} =~ ${coin[0]} ]]; then
+            echo ${coin[0]}
+            cleanerremoved=$($komodo_cli -ac_name=${coin[0]} cleanwallettransactions | jq -r .removed_transactions)
+#            if ($cleanerremoved > "0"); then
+                echo "$dt [cleanwallettransactions] ${coin[0]} - Removed $cleanerremoved transactions" >> $nntoolslogfile
+#            fi
+        fi
+    done
+fi
