5e5cdb8773422bbfcc5f315448945aafa9e7beea
jeezy <j33zy@pm.me>
2019-09-29 10:59:58 +0200

    modified:   cleanwallets.sh 	modified:   coinlist

--- cleanwallets.sh
+++ cleanwallets.sh
@@ -10,20 +10,30 @@ source $HOME/node.conf
 
 dt=$(date '+%Y-%m-%d %H:%M:%S');
 
+echo "KMD"
 $komodo_cli cleanwallettransactions | jq -r .removed_transactions
 #if ($cleanerremoved > 0); then
 #    echo "$dt [cleanwallettransactions] KMD - Removed $cleanerremoved transactions" >> $nntoolslogfile
 #fi
 
-if (( thirdpartycoins < 1 )); then
-    for coins in "${coinlist[@]}"; do
-        coin=($coins)
-        if [[ ! ${ignoreacs[*]} =~ ${coin[0]} ]]; then
-            echo ${coin[0]}
-            $komodo_cli -ac_name=${coin[0]} cleanwallettransactions | jq -r .removed_transactions
+$HOME/komodo/src/listassetchains | while read list; do
+	if [[ "${ignoreacs[@]}" =~ "${list}" ]]; then
+		continue
+        fi
+        echo $list
+        $komodo_cli -ac_name=$list cleanwallettransactions | jq -r .removed_transactions
+done
+
+
+#if (( thirdpartycoins < 1 )); then
+#    for coins in "${coinlist[@]}"; do
+#        coin=($coins)
+#        if [[ ! ${ignoreacs[*]} =~ ${coin[0]} ]]; then
+#            echo ${coin[0]}
+#            $komodo_cli -ac_name=${coin[0]} cleanwallettransactions | jq -r .removed_transactions
 #            if ($cleanerremoved > "0"); then
 #                echo "$dt [cleanwallettransactions] ${coin[0]} - Removed $cleanerremoved transactions" >> $nntoolslogfile
 #            fi
-        fi
-    done
-fi
+#        fi
+#    done
+#fi
